c79aa3fbc4c5a8440c618f3ce56339b5eeda62ab
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-29 21:28:27 +0800

    ADDED MORE THINGS

--- app/src/main/java/sg/edu/rp/c346/id20046797/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/practical2/MainActivity.java
@@ -3,12 +3,30 @@ package sg.edu.rp.c346.id20046797.practical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnJackabee, btnJackal, btnJavanese, btnJerboa, btnJellyfish, btnWolfhound, btnTerrier, btnIguana, btnElephant, btnTortoise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        btnJackabee
+        btnJackal = findViewById(R.id.buttonJackal);
+//        btnJavanese
+//        btnJerboa
+//        btnJellyfish
+//        btnWolfhound
+//        btnTerrier
+//        btnIguana
+//        btnElephant
+//        btnTortoise
+
+        // Reserved for Jackabee Demonstration
+
+        //
     }
 }
