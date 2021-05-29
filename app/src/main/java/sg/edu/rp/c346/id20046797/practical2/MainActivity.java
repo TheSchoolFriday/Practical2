f51bdc94c33e175b6932471b608fe75dc96ac914
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-29 23:12:37 +0800

    Added all the Buttons (no Functions Yet)
    Finished Jackal Template to be Copy Pasted for All other Activities.

--- app/src/main/java/sg/edu/rp/c346/id20046797/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/practical2/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,33 +19,91 @@ public class MainActivity extends AppCompatActivity {
 
 //        btnJackabee
         btnJackal = findViewById(R.id.buttonJackal);
-//        btnJavanese
-//        btnJerboa
-//        btnJellyfish
-//        btnWolfhound
-//        btnTerrier
-//        btnIguana
-//        btnElephant
-//        btnTortoise
+        btnJavanese = findViewById(R.id.buttonJavanese);
+        btnJerboa = findViewById(R.id.buttonJerboa);
+        btnJellyfish = findViewById(R.id.buttonJellyfish);
+        btnWolfhound = findViewById(R.id.buttonWolfhound);
+        btnTerrier = findViewById(R.id.buttonTerrier);
+        btnIguana = findViewById(R.id.buttonIguana);
+        btnElephant = findViewById(R.id.buttonElephant);
+        btnTortoise = findViewById(R.id.buttonTortoise);
 
         // Reserved for Jackabee Demonstration
 
-        // Reserved for Jackal.
+
+
         btnJackal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(MainActivity.this, Jackal.class);
-                startActivity(intent2);
+                Intent intent = new Intent(MainActivity.this, Jackal.class);
+                startActivity(intent);
             }
         });
 
-//        btnJavanese
-//        btnJerboa
-//        btnJellyfish
-//        btnWolfhound
-//        btnTerrier
-//        btnIguana
-//        btnElephant
-//        btnTortoise
+        btnJavanese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Yep I work.", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnJerboa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnJellyfish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnWolfhound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnTerrier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnIguana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnElephant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnTortoise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Jackal.class);
+//                startActivity(intent);
+            }
+        });
+
     }
 }
